f1cffa8b0ba48fb3414f40a618ba0e4c0a03ccdf
александр <shurik.ka4og@yandex.ru>
2022-02-28 17:06:40 +0300

    gjgj

--- js/main.js
+++ js/main.js
@@ -16,3 +16,46 @@ function stringLength (string, maxLength) {
 }
 
 stringLength ('Какая-то строка', 140);
+
+const NAMES = [
+  'Иван',
+  'Мария',
+  'Александр',
+  'Виктор',
+  'Юлия',
+  'Артем',
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.'
+];
+
+const creatPhoto = () => {
+  const randomId = getRandomPositiveInteger(1, 25);
+  const randomLikes = getRandomPositiveInteger(15, 200);
+  const randomIdComments = getRandomPositiveInteger(1, 999);
+  const randomAvatar = getRandomPositiveInteger(1, 6);
+  const randomMessageIndex = getRandomPositiveInteger(0, MESSAGE.length - 1);
+  const randomNameIndex = getRandomPositiveInteger(0, NAMES.length - 1);
+
+  return {
+    id: randomId,
+    url: `photos/${  randomId  }.jpg`,
+    description: '',
+    lakes: randomLikes,
+    comments: [
+      {
+        id: randomIdComments,
+        // eslint-disable-next-line no-useless-concat
+        avatar: 'img/avatar' + `-${  randomAvatar  }.svg`,
+        message: MESSAGE[randomMessageIndex],
+        name: NAMES[randomNameIndex]
+      },
+
+    ]
+  };
+
+};
+
+creatPhoto();
