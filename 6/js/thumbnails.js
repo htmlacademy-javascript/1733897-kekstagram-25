33476e7b86f2e80f5a225562f45c7e5d10f32ff2
Keks <keks@htmlacademy.ru>
2022-03-24 22:14:24 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/thumbnails.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import {createPhoto} from './data.js';
+
+const PHOTOS = 25;
+
+const picturesContainerElement = document.querySelector('.pictures');
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const documentFragment = document.createDocumentFragment();
+
+const createPhotos = [];
+
+for (let i = 0; i < PHOTOS; i++) {
+  createPhotos[i] = createPhoto();
+}
+
+createPhotos.forEach((photo) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  documentFragment.appendChild(pictureElement);
+});
+
+
+picturesContainerElement.appendChild(documentFragment);
+
+
---
+++ 6/js/util.js
@@ -0,0 +1,23 @@
+function getRandomPositiveInteger (min, max) {
+
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+getRandomPositiveInteger ();
+
+
+function stringLength (string, maxLength) {
+  return string.length <= maxLength;
+}
+
+stringLength ('Какая-то строка', 140);
+
+function isEscapeKey(evt) {
+  return evt.key === 'Escape';
+}
+
+
+export {getRandomPositiveInteger, stringLength, isEscapeKey};
